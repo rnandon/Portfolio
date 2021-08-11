b71820db592913e8862cc979fa66bb3544917a9b
Ryan <ryan@ryannd.com>
2021-08-11 08:36:14 -0500

    Restructured src folder, added files and folders for About, Contact, FooterBar, HeaderBar, and Work components

--- portfolio/src/components/App.js
+++ portfolio/src/components/App.js
@@ -23,3 +23,15 @@ function App() {
 }
 
 export default App;
+
+/*
+Idea for layout:
+
+Navbar. Sticky, shows name at all sizes. Collapses on mobile, maybe a different icon for the collapse button?
+Home page. A brief description of me, a couple highlights.
+About page. A more thorough description of me, what tech I like, etc.
+Work page. High level overviews of my work, links to github and video walkthroughs
+Blog page? Show most recent posts first. This might need a backend.
+Contact page. Form, on submit send me an email. Also provide contact info.
+
+*/
