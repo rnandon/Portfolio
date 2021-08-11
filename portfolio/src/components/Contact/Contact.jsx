98d7ff6b87b547bda4100d645813332f84686f4e
Ryan <ryan@ryannd.com>
2021-08-11 08:44:07 -0500

    Scaffolded basic structure for components

---
+++ portfolio/src/components/Contact/Contact.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './Contact.css'
+
+class Contact extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div className="container-fluid">
+                <h1>Under Construction!</h1>
+            </div>
+        )
+    }
+}
+
+export default Contact;
