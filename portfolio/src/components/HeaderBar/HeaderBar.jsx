e67c653880d9d069827324c08d24049d789873c2
Ryan <ryan@ryannd.com>
2021-08-12 12:24:36 -0500

    Implemented rough structure of HeaderBar component

--- portfolio/src/components/HeaderBar/HeaderBar.jsx
+++ portfolio/src/components/HeaderBar/HeaderBar.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
 import './HeaderBar.css';
 
 function HeaderBar() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar collapseOnSelect expand="md" bg="light" variant="light">
+      <Container>
+        <Navbar.Brand href="#Home">Ryan Nance-Donaldson</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#Home">Home</Nav.Link>
+            <Nav.Link href="#About">About</Nav.Link>
+            <NavDropdown title="Work" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Project One</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Project Two</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Project Three</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">All Projects</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#Contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
