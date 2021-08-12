30ecc8cffd5d030b89dfd55e876c87078f13d814
Ryan <ryan@ryannd.com>
2021-08-12 12:13:18 -0500

    Added and implemented SectionTitle component

---
+++ portfolio/src/components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './SectionTitle.css';
+
+const SectionTitle = (props) => {
+    return (<h1 className="separator-bar">{props.sectionName}</h1>);
+}
+
+export default SectionTitle;
