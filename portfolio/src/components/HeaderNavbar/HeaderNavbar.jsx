b994bbc17470af53e380a80a82deaa74bcdb2caa
Ryan <ryan@ryannd.com>
2021-08-12 12:28:04 -0500

    Moved HeaderNavbar to separate folder/files

---
+++ portfolio/src/components/HeaderNavbar/HeaderNavbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+import './HeaderNavbar.css';
+
+function HeaderNavbar() {
+  return (
+    <Navbar collapseOnSelect expand="md" bg="light" variant="light">
+      <Container>
+        <Navbar.Brand href="#Home">Ryan Nance-Donaldson</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#Home">Home</Nav.Link>
+            <Nav.Link href="#About">About</Nav.Link>
+            <NavDropdown title="Work" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Project One</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Project Two</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Project Three</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">All Projects</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#Contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default HeaderNavbar;
