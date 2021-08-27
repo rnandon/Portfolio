ec39d05bbaa1e69cc1f780f67a1c9b9988fd348e
Ryan Nance-Donaldson <ryan@ryannd.com>
2021-08-27 12:09:08 -0500

    Started implementing react-router, refactoring to suit

--- portfolio/src/components/App.js
+++ portfolio/src/components/App.js
@@ -1,58 +1,65 @@
-import React, {Component} from 'react';
+import React, { Fragment } from 'react';
+import { Router, Switch, Route } from 'react-router';
 import './App.css'
 import HeaderBar from './HeaderBar/HeaderBar'
 import About from './About/About'
 import Work from './Work/Work'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import Project from './Project/Project'
+import Contact from './Contact/Contact';
 import HeaderNavbar from './HeaderNavbar/HeaderNavbar';
 import ContactModal from './Contact/ContactModal'
 
-class App extends Component {
-    constructor(props){
-        super(props);
-    }
-
-    render(){
+const App = () => {
       return (
-        <Container className="m-0 p-0" fluid>
-            <HeaderNavbar />
-            
-            <Row className="mx-0">
-                <Col className="m-0 p-0 d-none d-md-block col-md-2 bg-dark" />
-                <Col className="m-0 p-0 col-xs-12 col-md-8" >
-                    <Row className="my-5 mx-0"/>
-                    <Row className="mx-0" >
-                        <HeaderBar />
-                    </Row>
-                    <Row className="mx-0" >
-                        <About />
-                    </Row>
-                    <Row className="mx-0" >
-                        <Work />
-                    </Row>
-                    <Row>
-                        <ContactModal />
-                    </Row>
-                </Col>
-                <Col className="m-0 p-0 d-none d-md-block col-md-2 bg-dark" />
-            </Row>
-        </Container>
+        <Router>
+            <main>
+                <HeaderNavbar />
+                <HeaderBar />
+                <Switch>
+                    <Route path="/" exact component={HomePage} />
+                    <Route path="/about/" component={AboutPage} />
+                    <Route path="/contact/" component={ContactPage} />
+                    <Route path="/work/" component={WorkPage} />
+                    <Route path="/work/:project" component={ProjectPage} />
+                </Switch>
+            </main>
+        </Router>
       )
-    }
+}
+
+const HomePage = () => {
+    <Fragment>
+        <h1>Home</h1>
+        <Home />
+    </Fragment>
+}
+
+const AboutPage = () => {
+    <Fragment>
+        <h1>About</h1>
+        <About />
+    </Fragment>
+}
+
+const ContactPage = () => {
+    <Fragment>
+        <h1>Contact</h1>
+        <Contact />
+    </Fragment>
+}
+
+const WorkPage = () => {
+    <Fragment>
+        <h1>Work</h1>
+        <Work />
+    </Fragment>
+}
+
+const ProjectPage = ({match:{params:{project}}}) => {
+    <Fragment>
+        <h1>Project</h1>
+        <Project project={project} />
+    </Fragment>
 }
 
 export default App;
-
-/*
-Idea for layout:
-
-Navbar. Sticky, shows name at all sizes. Collapses on mobile, maybe a different icon for the collapse button?
-Home page. A brief description of me, a couple highlights.
-About page. A more thorough description of me, what tech I like, etc.
-Work page. High level overviews of my work, links to github and video walkthroughs
-Blog page? Show most recent posts first. This might need a backend.
-Contact page. Form, on submit send me an email. Also provide contact info.
-
-*/
