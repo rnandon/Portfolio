8a4a5d58a2efe13e345a8d09ba30621195e8d3e3
Ryan <ryan@ryannd.com>
2021-10-13 13:11:46 -0500

    Added more projects to work page

--- portfolio/src/components/Project/Project.jsx
+++ portfolio/src/components/Project/Project.jsx
@@ -7,22 +7,23 @@ import ProjectImage from './ProjectImage/ProjectImage';
 function Project(props) {
   return (
     <div>
-      <div className="container">
-        <div className="row">
-          <div className="col text-center">
-            <ProjectTitle name={props.project.name} />
-          </div>    
+        <div className="container">
+            <div className="row">
+                <div className="col text-center">
+                    <ProjectTitle name={props.project.name} />
+                </div>    
+                </div>
+                <div className="row">
+                <div className="col-sm-12 col-md-5">
+                    <ProjectImage imgSrc={`./${props.project.imgSrc}`} />
+                </div>
+                <div className="col-sm-12 col-md-7">
+                    <ProjectDescription description={props.project.description} />
+                    <a href={props.project.githubUrl} className="btn btn-secondary">Github</a>
+                    {props.project.videoUrl && <a href={props.project.videoUrl} className="btn btn-secondary">Video Demo</a>}
+                </div>
+            </div>
         </div>
-        <div className="row">
-          <div className="col-sm-12 col-md-5">
-            <ProjectImage imgSrc={`./${props.project.imgSrc}`} />
-          </div>
-          <div className="col-sm-12 col-md-7">
-            <ProjectDescription description={props.project.description} />
-            <a href={props.project.githubUrl} className="btn btn-secondary">Github</a>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
